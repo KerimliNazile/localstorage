1cbc643f5f9e0d44f66415a6dc1d54299bae8466
ismayilhsnv <tu7u02d2s@code.edu.az>
2023-10-30 13:39:12 +0400

    added

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+// localStorage.setItem('ad','Nazile')
+//  let ad=localStorage.getItem('ad')
+
+
+// localStorage.setItem('soyad','Kerimli')
+// let soyad=localStorage.getItem('soyad')
+
+
+// localStorage.setItem('username','Nazilya4')
+// let username=localStorage.getItem('username')
+
+
+// localStorage.setItem('password','1406')
+// let password=localStorage.getItem('password')
+
+
+
+
+// const p=document.createElement('p')
+// const body=document.createElement("body")
+
+
+
+// p.innerHTML=localStorage.getItem("ad")+" ", localStorage.getItem("soyad")+" ", localStorage.getItem("username")+"",localStorage.getItem("password")+"";
+
+// body.append(p);
+// console.log(p);
+
+// task2
+
+// const input=document.querySelector('input')
+// const container=document.querySelector('container')
+// const click=document.getElementById('click')
+// const list =document.getElementById('list')
+
+// click.addEventListener("click",()=>{
+//     e.preventDefault();
+//     if(input.value===""){
+//         return;
+//     }
+    
+//     const li=document.createElement("li")
+//     const inp=document.createElement("input")
+
+//     inp.setAttribute("type","checkbox")
+
+//     li.textContent=input.value
+//     inp.onclick=()=>
+//     inp.checked ? li.classList.add("xett"): li.classList.remove("xett");
+//     li.appendChild(inp)
+//     list.appendChild(li)
+
+//     input.value=" ";
+// });
+
+
+// const username=document.getElementById("username")
+// const password=document.getElementById("password")
+// const save=document.getElementById("save")
+
+// save.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     const username1=username.value
+//      const password1=password.value
+
+//     obj={
+//         name:username1,
+//         parol:password1
+//     }
+
+
+//     localStorage.setItem("obyekt",JSON.stringify(obj))
+//     const element=JSON.parse(localStorage.getItem("obyekt"))
+
+//     console.log(element);
+// })
+
